498a616c98c7ec09764d9ee24845e8c688c811c4
FinnbarrAmbrose <finnbarr.ambrose121@gmail.com>
2025-06-29 09:05:12 +0000

    Refactor setup script to create output directories and check for pre-generated files

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
-mkdir -p ~/.streamlit/
-echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
-" > ~/.streamlit/config.toml
+#!/usr/bin/env bash
+set -e
+
+echo "→ creating output dirs"
+mkdir -p ../outputs/datasets/collection outputs/models
+
+echo "→ using pre-generated CSV & model"
+ls ../outputs/datasets/collection/*.csv || echo "no CSV!"
+ls ../outputs/models/*.pkl             || echo "no model!"
